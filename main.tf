69975005b1c65ef2bf21df74df458b75aa36d9ff
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-08-27 15:34:19 -0300

    fix: change name to name prefix and create before destroy sg to work with ec2 module

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_security_group" "this" {
-  name        = var.name
+  name_prefix = var.name
   description = var.description
   vpc_id      = var.vpc_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   dynamic "ingress" {
     for_each = var.ingress_rules
